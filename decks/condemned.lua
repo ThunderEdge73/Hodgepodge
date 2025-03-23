e4a9f43c6bb2bc6b8cdaffc622c1a264c5316b1d
PokeRen <wnap19s@royallatin.org>
2025-03-23 18:52:42 +0000

    Condemned deck changes

--- decks/condemned.lua
+++ decks/condemned.lua
@@ -7,7 +7,8 @@ SMODS.Back {
         name = "Condemned Deck",
         text = {
             "Random cards are missing",
-            "Random cards are {C:attention,T:m_rendom_asbestos}Asbestos{}"
+            "Random cards are {C:attention,T:m_rendom_asbestos}Asbestos{}",
+            "Random cards are {C:attention,T:m_rendom_waterdamage}Water Damaged{}"
         }
     },
     apply = function()
@@ -17,8 +18,10 @@ SMODS.Back {
                     local rand = math.random()
                     if rand < 0.2 then
                         G.playing_cards[i]:remove()
-                    elseif rand < 0.6 then
+                    elseif rand < 0.35 then
                         G.playing_cards[i]:set_ability(G.P_CENTERS.m_rendom_asbestos)
+                    elseif rand < 0.6 then
+                        G.playing_cards[i]:set_ability(G.P_CENTERS.m_rendom_waterdamage)
                     end
                 end
                 return true
