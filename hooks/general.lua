52954bfedaa865d43a68e8ebcf60b6ca3cec18c4
PokeRen <wnap19s@royallatin.org>
2025-04-06 17:49:19 +0100

    Added title screen card

--- hooks/general.lua
+++ hooks/general.lua
@@ -63,3 +63,35 @@ function Card:calculate_seal(context)
     end
     return ret
 end
+
+-- Menu card
+local menuHook = Game.main_menu
+function Game:main_menu(ctx)
+    local r = menuHook(self,ctx)
+    local center = pseudorandom_element({"rendom_SUNS_A","rendom_MOONS_A","rendom_SNAKE_A"},pseudoseed("rendom_titlecard"))
+    local card = Card(0,0,G.CARD_W,G.CARD_H,G.P_CARDS[center],G.P_CENTERS.m_rendom_asbestos)
+    card.T.w = card.T.w * 1.4
+    card.T.h = card.T.h * 1.4
+    G.title_top.T.w = G.title_top.T.w * 1.7675
+    G.title_top.T.x = G.title_top.T.x - 0.8
+    card:set_sprites(card.config.center)
+    card.no_ui = true
+    card.states.visible = false
+    self.title_top:emplace(card)
+    G.E_MANAGER:add_event(
+        Event{
+            delay = 0.5,
+            func = function()
+                if ctx == "splash" then
+                    card.states.visible = true
+                    card:start_materialize({G.C.WHITE, G.C.WHITE}, true, 2.5)
+                else
+                    card.states.visible = true
+                    card:start_materialize({G.C.WHITE, G.C.WHITE}, nil, 1.2)
+                end
+                return true
+            end
+        }
+    )
+    return r
+end
